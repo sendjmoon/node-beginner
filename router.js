2dd09dd9f1e87a38fd9e848888ded7b613bb19f1
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-07-17 15:39:07 -0700

    added request handlers

--- router.js
+++ router.js
@@ -1,5 +1,10 @@
-function route(pathname) {
+function route(handle, pathname) {
   console.log('about to route a request for ' + pathname);
+  if(typeof handle[pathname] === 'function') {
+    handle[pathname]();
+  } else {
+    console.log('No request found for ' + pathname);
+  }
 }
 
 exports.route = route;
--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 var http = require('http');
 var url = require('url');
 
-function start(route) {
+function start(route, handle) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
-    console.log('request for ' + pathname + ' received');
+    if(pathname !== '/favicon.ico') {
+      console.log('request for ' + pathname + ' received');
 
-    route(pathname);
+      route(handle, pathname);
 
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('hello world');
-    response.end();
+      response.writeHead(200, {'Content-Type': 'text/plain'});
+      response.write('hello world');
+      response.end();
+    }
   }
 
   http.createServer(onRequest).listen(8888);

9c652277d4be1c11bdcd8fe8dc041b48c148e5f7
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-07-17 17:07:10 -0700

    modified index.js, requestHandlers,js, router.js, and server.js to use formidable to handle upload requests

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,17 +1,18 @@
 var querystring = require('querystring');
+var fs = require('fs');
+var formidable = require('formidable');
 
-function start(response, postData) {
-  console.log('request handler \'start\' was called.');
+function start(response) {
+  console.log('Request handler \'start\' was called.');
 
   var body = '<html>' +
   '<head>' +
-  '<meta http-equiv="Content-Type" content="text/html; ' +
-  'charset=UTF-8" />' +
+  '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />' +
   '</head>' +
   '<body>' +
-  '<form action="/upload" method="post">' +
-  '<textarea name="text" rows="20" cols="60"></textarea>' +
-  '<input type="submit" value="Submit text" />' +
+  '<form action="/upload" enctype="multipart/form-data" method="post">' +
+  '<input type="file" name="upload" multiple="multiple">' +
+  '<input type="submit" value="Upload file" />' +
   '</form>' +
   '</body>' +
   '</html>';
@@ -21,12 +22,32 @@ function start(response, postData) {
   response.end();
 }
 
-function upload(response, postData) {
-  console.log('request handler \'upload\' was called.');
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write('You\'ve sent the text: ' + querystring.parse(postData).text);
-  response.end();
+function upload(response, request) {
+  console.log('Request handler \'upload\' was called.');
+
+  var form = new formidable.IncomingForm();
+  console.log('about to parse');
+  form.parse(request, function(error, fields, files) {
+    console.log('parsing done');
+    fs.rename(files.upload.path, '/tmp/test.gif', function(error) {
+      if (error) {
+        fs.unlink('/tmp/test.gif');
+        fs.rename(files.upload.path, '/tmp/test.gif');
+      }
+    });
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write('received image:<br/>');
+    response.write('<img src="/show" />');
+    response.end();
+  });
+}
+
+function show(response) {
+  console.log('Request handler \'show\' was called.');
+  response.writeHead(200, {'Content-Type': 'image/gif'});
+  fs.createReadStream('/tmp/test.gif').pipe(response);
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
